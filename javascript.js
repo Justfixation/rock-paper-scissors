4d402a6eeb01173d600da99f9eb2e40bf5f2b52b
Kian <Kian.berg333@gmail.com>
2023-09-24 22:44:16 -0600

    Replace fiveGameLoop() with an internal while loop
    Added intentional errors to stop code when triggering "Oops, something
    went wrong!" lines.

--- javascript.js
+++ javascript.js
@@ -47,67 +47,69 @@ function getPlayerChoice(){
     }
     
 }
+
+let roundCounter = 1;
+
+function playGame(computerChoice, playerChoice){
+    if(computerChoice == "Rock") {
+        if(playerChoice == "Rock") {
+            console.log(`Round ${roundCounter} result = tie`);
+            alert(`Round ${roundCounter} is a tie! The computer picked ${computerChoice}.`);
+        } else if(playerChoice == "Paper") {
+            console.log(`Round ${roundCounter} result = win`);
+            alert(`Congratulations, you won round ${roundCounter}! The computer picked ${computerChoice}.`)
+        } else if(playerChoice == "Scissors") {
+            console.log(`Round ${roundCounter} result = loss`);
+            alert(`You lost round ${roundCounter}, how unfortunate! The computer picked ${computerChoice}.`)
+        } else {
+            alert("Oops, something went wrong with playerChoice!");
+            throw new Error("");
+        }
+    } else if (computerChoice == "Paper") {
+        if(playerChoice == "Rock") {
+            console.log(`Round ${roundCounter} result = loss`);
+            alert(`You lost round ${roundCounter}, how unfortunate! The computer picked ${computerChoice}.`)
+        } else if(playerChoice == "Paper") {
+            console.log(`Round ${roundCounter} result = tie`);
+            alert(`Round ${roundCounter} is a tie! The computer picked ${computerChoice}.`);
+        } else if(playerChoice == "Scissors") {
+            console.log(`Round ${roundCounter} result = win`);
+            alert(`Congratulations, you won round ${roundCounter}! The computer picked ${computerChoice}.`)
+        } else {
+            alert("Oops, something went wrong with playerChoice!");
+            throw new Error("");
+        }
+    } else if (computerChoice == "Scissors") {
+        if(playerChoice == "Rock") {
+            console.log(`Round ${roundCounter} result = win`);
+            alert(`Congratulations, you won round ${roundCounter}! The computer picked ${computerChoice}.`)
+        } else if(playerChoice == "Paper") {
+            console.log(`Round ${roundCounter} result = loss`);
+            alert(`You lost round ${roundCounter}, how unfortunate! The computer picked ${computerChoice}.`)
+        } else if(playerChoice == "Scissors") {
+            console.log(`Round ${roundCounter} result = tie`);
+            alert(`Round ${roundCounter} is a tie! The computer picked ${computerChoice}.`);
+        } else {
+            alert("Oops, something went wrong with playerChoice!");
+            throw new Error("");
+        }
+    } else {
+        console.log("Oops, something went wrong with computerChoice!");
+        console.log(computerChoice);
+        throw new Error("");
+    }
+    roundCounter += 1;
+    while (roundCounter <= 5) {
+        playGame(getComputerChoice(), getPlayerChoice());
+    }
+    /* including a while loop within playGame() that calls playGame() is ingenious! as long as the
+    roundCounter increase happens before the next repeat playGame(), it limits the number of runs to
+    the designated amount! */
+
+    /* I need to store the previous game results and print them all together after each round. */
+}
+
+playGame(getComputerChoice(), getPlayerChoice());
 /* Including getComputerChoice() and getPlayerChoice() as parameters for playGame()
 allows for both functions to be called only at the time of playGame(), instead of calling
 all three separately.*/
-function playGame(computerChoice, playerChoice){
-    let roundCounter = 1;
-        if(computerChoice == "Rock") {
-            if(playerChoice == "Rock") {
-                console.log(`Round ${roundCounter} result = tie`);
-                alert(`Round ${roundCounter} is a tie! The computer picked ${computerChoice}.`);
-            } else if(playerChoice == "Paper") {
-                console.log(`Round ${roundCounter} result = win`);
-                alert(`Congratulations, you won round ${roundCounter}! The computer picked ${computerChoice}.`)
-            } else if(playerChoice == "Scissors") {
-                console.log(`Round ${roundCounter} result = loss`);
-                alert(`You lost round ${roundCounter}, how unfortunate! The computer picked ${computerChoice}.`)
-            } else {
-                alert("Oops, something went wrong with playerChoice!");
-            }
-
-        } else if (computerChoice == "Paper") {
-            if(playerChoice == "Rock") {
-                console.log(`Round ${roundCounter} result = loss`);
-                alert(`You lost round ${roundCounter}, how unfortunate! The computer picked ${computerChoice}.`)
-            } else if(playerChoice == "Paper") {
-                console.log(`Round ${roundCounter} result = tie`);
-                alert(`Round ${roundCounter} is a tie! The computer picked ${computerChoice}.`);
-            } else if(playerChoice == "Scissors") {
-                console.log(`Round ${roundCounter} result = win`);
-                alert(`Congratulations, you won round ${roundCounter}! The computer picked ${computerChoice}.`)
-            } else {
-                alert("Oops, something went wrong with playerChoice!");
-            }
-
-        } else if (computerChoice == "Scissors") {
-            if(playerChoice == "Rock") {
-                console.log(`Round ${roundCounter} result = win`);
-                alert(`Congratulations, you won round ${roundCounter}! The computer picked ${computerChoice}.`)
-            } else if(playerChoice == "Paper") {
-                console.log(`Round ${roundCounter} result = loss`);
-                alert(`You lost round ${roundCounter}, how unfortunate! The computer picked ${computerChoice}.`)
-            } else if(playerChoice == "Scissors") {
-                console.log(`Round ${roundCounter} result = tie`);
-                alert(`Round ${roundCounter} is a tie! The computer picked ${computerChoice}.`);
-            } else {
-                alert("Oops, something went wrong with playerChoice!");
-            }
-
-        } else {
-            console.log("Oops, something went wrong with computerChoice!");
-            console.log(computerChoice);
-        }
-}
-
-function fiveGameLoop() {
-    let roundCounter = 1;
-    while (roundCounter <= 5) {
-        playGame(getComputerChoice(), getPlayerChoice());
-        roundCounter += 1;
-        console.log(roundCounter);
-    }
-}
-/* Need to separate roundCounter so that it updates properly */
-
-fiveGameLoop();
